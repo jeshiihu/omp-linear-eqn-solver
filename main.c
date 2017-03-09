c981a813e85eda4e155822b29c967379d3e7748d
Jessica Huynh <jhuynh@ualberta.ca>
2017-03-09 14:06:18 -0700

    copy the serial tester stuff

--- main.c
+++ main.c
@@ -4,4 +4,58 @@
 
 int main(int argc, char* argv[]) {
 	
+	int i, j, k;
+	double **Au;
+    double *X;
+    double temp;
+	int size; 
+	int *index;
+
+	// load input and create the unknown vector X
+    Lab3LoadInput(&Au, &size);
+    X = CreateVec(size);
+
+	index = malloc(size * sizeof(int));
+    for (i = 0; i < size; ++i)
+        index[i] = i;
+
+    if (size == 1)
+        X[0] = Au[0][1] / Au[0][0];
+    else{
+        /*Gaussian elimination*/
+        for (k = 0; k < size - 1; ++k){
+            /*Pivoting*/
+            temp = 0;
+            for (i = k, j = 0; i < size; ++i)
+                if (temp < Au[index[i]][k] * Au[index[i]][k]){
+                    temp = Au[index[i]][k] * Au[index[i]][k];
+                    j = i;
+                }
+
+            if (j != k)/*swap*/{
+                i = index[j];
+                index[j] = index[k];
+                index[k] = i;
+            }
+
+            /*calculating*/
+            for (i = k + 1; i < size; ++i){
+                temp = Au[index[i]][k] / Au[index[k]][k];
+                for (j = k; j < size + 1; ++j)
+                    Au[index[i]][j] -= Au[index[k]][j] * temp;
+            }       
+        }
+
+        /*Jordan elimination*/
+        for (k = size - 1; k > 0; --k){
+            for (i = k - 1; i >= 0; --i ){
+                temp = Au[index[i]][k] / Au[index[k]][k];
+                Au[index[i]][k] -= temp * Au[index[k]][k];
+                Au[index[i]][size] -= temp * Au[index[k]][size];
+            } 
+        }
+
+         for (k=0; k< size; ++k)
+            X[k] = Au[index[k]][size] / Au[index[k]][k];
+    }
 }
